acc6ddf6c6871ab26a2d6c64f69a49d49f381ae8
Robert Duffy <robduffy@gmail.com>
2020-10-27 21:30:41 -0700

    New footer action links

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,25 +5,34 @@ const Footer = () => (
     <section className="section footer">
         <div className="container">
             <div className="row">
-                <div className="col-lg-12">
-                    <div className="text-center">
-                        <ul className="list-inline social mb-0">
-                            <li className="list-inline-item"><a href="https://" className="social-icon text-muted"><i className="ti-facebook"></i></a></li>
-                            <li className="list-inline-item"><a href="#" className="social-icon text-muted"><i className="ti-twitter-alt"></i></a></li>
-                            <li className="list-inline-item"><a href="#" className="social-icon text-muted"><i className="ti-linkedin"></i></a></li>
-                        </ul>
-                    </div>
-                    <div className="footer-terms">
-                        <ul className="mb-0 list-inline text-center mt-4 pt-2">
-                            <li className="list-inline-item"><a href="#" className="text-muted">Terms & Condition</a></li>
-                            <li className="list-inline-item"><a href="#" className="text-muted">Privacy Policy</a></li>
-                            <li className="list-inline-item"><a href="#" className="text-muted">Contact Us</a></li>
-                        </ul>
-                    </div>
-                    <div className="mt-4 pt-2 text-center">
-                        <p className="copy-rights text-muted mb-0"><script>document.write(new Date().getFullYear())</script> © Managed Commerce Inc. a Delaware Corporation.</p>
-                    </div>
-                </div>
+                  <div className="col-lg-6">
+                      <ul className="list-inline social mb-0">
+                          <li className="list-inline-item"><a href="https://" className="social-icon text-muted"><i className="ti-facebook"></i></a></li>
+                          <li className="list-inline-item"><a href="#" className="social-icon text-muted"><i className="ti-twitter-alt"></i></a></li>
+                          <li className="list-inline-item"><a href="#" className="social-icon text-muted"><i className="ti-linkedin"></i></a></li>
+                      </ul>
+                      <p className="copy-rights text-muted mt-5">© Managed Commerce Inc. a Delaware Corporation.</p>
+                  </div>
+                  <div className="col-lg-3">
+
+                      <h6 class="text-white-50">Legal stuff</h6>
+                      <ul className="list-unstyled">
+                          <li><a href="#" className="text-muted">Terms & Condition</a></li>
+                          <li><a href="#" className="text-muted">Privacy Policy</a></li>
+                      </ul>
+                      <h6 class="text-white-50 pt-2">Company stuff</h6>
+                      <ul className="list-unstyled">
+                          <li><a href="#" className="text-muted">Contact Us</a></li>
+                          <li><a href="#" className="text-muted">Careers</a></li>
+                      </ul>
+                  </div>
+                  <div className="col-lg-3">
+                      <h6 class="text-white-50">Sign up for our services</h6>
+                      <ul className="list-unstyled">
+                          <li><a href="#" id="launchFreeTrial" className="text-muted">Try Unlimited for free</a></li>
+                          <li><a href="#" id="contactEnterprise" className="text-muted">Start an Enterprise plan</a></li>
+                      </ul>
+                  </div>
             </div>
         </div>
     </section>
