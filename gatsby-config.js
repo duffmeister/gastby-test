7c4a2230ee725d92d28ebe254a3603fef778240e
Robert Duffy <robduffy@gmail.com>
2020-10-27 18:09:03 -0700

    Try static site

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,7 @@ module.exports = {
         includeInDevelopment: true,
       },
     },
+    `@wardpeet/gatsby-plugin-static-site`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
