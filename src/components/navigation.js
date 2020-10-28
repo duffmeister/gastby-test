2fb011151ff06023344a0871d7167148904c7cdf
Robert Duffy <robduffy@gmail.com>
2020-10-27 17:26:26 -0700

    Try new link

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -40,7 +40,7 @@ const Navigation = () => (
             <li className="list-inline-item">
                 <Link to="https://mcco.zendesk.com/hc" className="nav-link" activeClassName="nav-link active">Sign In</Link>
             </li>
-            <li className="list-inline-item"><a href="#" className="btn btn-cta intercom">Try for free</a></li>
+            <li className="list-inline-item"><a href="#" className="btn btn-cta intercom" id="launchChat">Try for free</a></li>
           </ul>
         </div>
       </div>
--- src/pages/lp/unlimited-plans.js
+++ src/pages/lp/unlimited-plans.js
@@ -64,6 +64,7 @@ const UnlimitedPage = ({data}) => (
         <Img fluid={data.placeholderImage.childImageSharp.fluid} />
       }
       cta="Try for free"
+      ctaLink="javascript:document.getElementById('launchChat').click();"
       bullets={[
         {icon: "ti-bar-chart", text: "Improve your conversion rates and performance"},
         {icon: "ti-check-box", text: "Fix bugs and design issues"},
@@ -140,6 +141,7 @@ const UnlimitedPage = ({data}) => (
     <CTA
       heading="Take us for a spin, create a free task..."
       cta="Try for free"
+      ctaLink="#"
     />
   </Layout>
 )
